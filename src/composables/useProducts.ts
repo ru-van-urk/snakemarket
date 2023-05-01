1391571f03dfd5e420dc496079a15671ac0e607f
joxy379 <36665384+ru-van-urk@users.noreply.github.com>
2023-05-01 18:48:17 +0200

    fixed state bug

--- src/composables/useProducts.ts
+++ src/composables/useProducts.ts
@@ -7,6 +7,7 @@ const useProducts = async () => {
   const config = useRuntimeConfig();
 
   return useFetch("https://api.dekamarkt.nl/v1/assortmentcache/group/281/104", {
+    key: "products",
     query: { api_key: config.public.dekaApiKey },
     transform: (res) => z.array(productSchema).parse(res),
   });
